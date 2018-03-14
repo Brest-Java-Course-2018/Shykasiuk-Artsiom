55d9f80537d3dd91c6576d6af43ac25617a511db
artemoxa <artemoxa0@gmail.com>
2018-03-14 20:52:39 +0300

    hotfix

--- web-app/src/main/java/com/epam/brest/course/web_app/controllers/DepartmentController.java
+++ web-app/src/main/java/com/epam/brest/course/web_app/controllers/DepartmentController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Collection;
-import java.util.Collections;
 
 /**
  * Department controller.
