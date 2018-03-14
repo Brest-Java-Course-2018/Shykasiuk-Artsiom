ed1e3dccfd5a5b3d8a78b7fc4ca4b9a25e41cbbe
artemoxa <artemoxa0@gmail.com>
2018-03-14 20:47:53 +0300

    Done controllers for editing and loading from db

--- service/src/main/java/com/epam/brest/course/service/DepartmentService.java
+++ service/src/main/java/com/epam/brest/course/service/DepartmentService.java
@@ -1,14 +1,24 @@
 package com.epam.brest.course.service;
 
 import com.epam.brest.course.model.Department;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 /**
  * Department Service Interface.
  */
+@Service
 public interface DepartmentService {
 
     Department getDepartmentById(Integer departmentId);
 
+    /**
+     * Get list of departments.
+     *
+     * @return List of objects Department
+     */
+    Collection<Department> getDepartments();
     void updateDepartmentDescription(Integer departmentId, String description);
 
 }
--- web-app/src/main/java/com/epam/brest/course/web_app/controllers/DepartmentController.java
+++ web-app/src/main/java/com/epam/brest/course/web_app/controllers/DepartmentController.java
@@ -1,8 +1,15 @@
 package com.epam.brest.course.web_app.controllers;
 
+import com.epam.brest.course.model.Department;
+import com.epam.brest.course.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Department controller.
@@ -10,13 +17,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DepartmentController {
 
+    @Autowired
+    DepartmentService departmentService;
+
     @GetMapping(value = "/departments")
     public String departments(Model model) {
+        Collection<Department> departments =
+                departmentService.getDepartments();
+
+
+        model.addAttribute("departments", departments);
         return "departments";
     }
 
     @GetMapping(value = "/department")
     public String department(Model model) {
+        return "department";
+    }
+
+    @GetMapping(value = "/department/{id}")
+    public String getDepartmentById(@PathVariable Integer id,
+                                    Model model) {
+        Department department = departmentService.getDepartmentById(id);
+        model.addAttribute("department", department);
         return "department";
     }
 }
